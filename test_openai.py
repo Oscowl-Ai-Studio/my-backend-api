e7324f2d62bd1d90eb231ac3d254d6be6d892edd
Umar Khaja <sumarkhaja@gmail.com>
2026-05-22 15:03:31 +0530

    updated

--- test_openai.py
+++ test_openai.py
@@ -2,9 +2,9 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 load_dotenv()
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
-)
+# client = OpenAI(
+#     api_key=os.getenv("OPENAI_API_KEY")
+# )
 
 response = client.chat.completions.create(
     model="gpt-4.1-mini",
